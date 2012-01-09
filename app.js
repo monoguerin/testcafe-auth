0d3be7aaafd6a527b2c4534cbc0aee44df954276
Scott Mebberson <scott@scottmebberson.com>
2012-01-10 09:15:55 +1030

    Created a set of accessible and secure URLs

--- app.js
+++ app.js
@@ -4,9 +4,31 @@ var port = 8999;
 
 var app = express.createServer();
 
-app.get('/', function (req, res, next) {
-	res.send('Welcome');
+function checkAuth (req, res, next) {
+	console.log('checkAuth ' + req.url);
+
+	// don't serve /secure to those not logged in
+	if (req.url === '/secure' && (!req.session || !req.session.authenticated)) {
+		res.render('unauthorised', { status: 403 });
+		return;
+	}
+
+	next();
+}
+
+app.configure(function () {
+
+	app.use(express.cookieParser());
+	app.use(express.session({ secret: 'example' }));
+	app.use(express.bodyParser());
+	app.use(checkAuth);
+	app.use(app.router);
+	app.set('view engine', 'jade');
+	app.set('view options', { layout: false });
+
 });
+
+require('./lib/routes.js')(app);
 
 app.listen(port);
 console.log('Node listening on port %s', port);
