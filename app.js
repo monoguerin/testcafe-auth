1ed566089847619447d3f5ddf93f470dc981c861
Scott Mebberson <scott@scottmebberson.com>
2012-01-09 21:12:16 +1030

    Created basic server to respond to /

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ var port = 8999;
 var app = express.createServer();
 
 app.get('/', function (req, res, next) {
-
-res.send('Welcome');
-
+	res.send('Welcome');
 });
 
 app.listen(port);
+console.log('Node listening on port %s', port);
