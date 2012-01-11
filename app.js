d2512fcc0a15eb603d8ec89211b4cbd7bbbfe0a4
Scott Mebberson <scott@scottmebberson.com>
2012-01-11 14:07:18 +1030

    Completed the custom authentication example for Express

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ function checkAuth (req, res, next) {
 	console.log('checkAuth ' + req.url);
 
 	// don't serve /secure to those not logged in
+	// you should add to this list, for each and every secure url
 	if (req.url === '/secure' && (!req.session || !req.session.authenticated)) {
 		res.render('unauthorised', { status: 403 });
 		return;
