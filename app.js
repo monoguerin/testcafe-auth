6e9f03ff79aa9bc466eab74d5a0c1b90e7f61aba
Cesar Guerrero <cesar.guerrero@morningstar.com>
2017-12-13 12:51:46 -0500

    race condition option select

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function checkAuth (req, res, next) {
 
 	// don't serve /secure to those not logged in
 	// you should add to this list, for each and every secure url
-	if ((req.url === '/secure' || req.url === '/breeds') && (!req.session || !req.session.authenticated)) {
+	if ((req.url === '/secure') && (!req.session || !req.session.authenticated)) {
 		res.render('unauthorised', { status: 403 });
 		return;
 	}
--- static/test.js
+++ static/test.js
@@ -10,3 +10,16 @@ axios.get('/breeds')
   .catch(function (error) {
     console.log(error);
   });
+
+  axios.get('/breeds')
+  .then(function (response) {
+    console.log(response);
+    var breeds = response.data.message;
+    var breedHtml = breeds.map(function(breed) {
+      return "<option value='" + breed + "'>" + breed + "</option>";
+    });
+    $("form").append("<select name='selected'>" + breedHtml.join("") + "</select>");
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
